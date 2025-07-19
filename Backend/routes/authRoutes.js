b9b1c15f63449aabe92df375402b742d33535581
mrHarshchoudhary <Harshchoudharyv@gmail.com>
2025-07-19 09:44:47 +0530

    profile

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const authController = require('../controllers/authController');
-
-// // Login route
-// router.post('/login', authController.login);
-
-// // Profile route
-// router.get('/profile/:id', authController.getProfile);
-
-// module.exports = router;
-
 
 
 const express = require('express');
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
